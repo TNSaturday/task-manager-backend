98adf4e16a4ba5ba0068e0261d2fae48528fd515
TSubbota <tsubbota@nativeroll.tv>
2020-12-11 13:14:37 +0300

    Restructure code base, add user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 // mongod -dbpath="C:\Program Files\mongodb-data"
 
@@ -7,42 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-});
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true,
-        validate(value) {
-            if (value < 1) {
-                throw new Error('Age must be at least one!');
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Error is incorrect!');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"!');
-            }
-        }
-    }
 });
 
 const Task = mongoose.model('Task', {
@@ -56,21 +19,4 @@ const Task = mongoose.model('Task', {
         required: false,
         default: false
     },
-});
-
-// const user = new User({
-//     name: 'Roma',
-//     age: 30,
-//     email: 'aloneinbox@mail.ru',
-//     password: 'testtest222'
-// });
-
-const task = new Task({
-    description: 'Buy new keyboard'
-});
-
-task.save().then(() => {
-    console.log(task);
-}).catch((error) => {
-    console.log('Error!', error);
 });
